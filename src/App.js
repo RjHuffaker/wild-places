e40d78e1ce98166f5c0634ac700c75ca3f13918c
RjH <ryanjayhuffaker@gmail.com>
2022-12-13 09:22:34 -0700

    Loading placeList from JSON file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React, { useState } from 'react';
-
 import MapComponent from './components/MapComponent';
+import placeList from './placeList';
 
 function App() {
 
-  const [placesList, setPlacesList] = useState([
-    { title: "Place 1", description: "asdffdsa", position: { lat: 40.54149320752432, lng: -112.31041498069737 }, id: 1 },
-    { title: "Place 2", description: "rewqrew", position: { lat: 40.52149320752432, lng: -112.32051498069727 }, id: 2 },
-    { title: "Place 3", description: "rewq", position: { lat: 40.55963900732105, lng: -112.30175993300523 }, id: 3 },
-    { title: "Place 4", description: "gdsagf", position: { lat: 40.52963900733105, lng: -112.29165993301523 }, id: 4 },
-    { title: "Place 5", description: "ttrewtre", position: { lat: 40.56963900734105, lng: -112.33195993302523 }, id: 5 }
-  ]);
+  const [placesList, setPlacesList] = useState(placeList);
+
+  console.log(placeList);
 
   const onPlaceSubmit = (placeData) => {
     let newPlace = placeData;
     newPlace.id = placesList.length+1;
     placesList.push(placeData);
     setPlacesList(placesList);
-    console.log(placesList);
+    console.log(JSON.stringify(placesList, null, 2));
   }
 
   return (
